40f1aac9cfab72b045e98daad477107bd4f6a360
AlexeyPas <lexa211298@gmail.com>
2020-11-22 16:33:45 +0300

    Finished UI

--- Quiz 2020/View Controllers/WelcomViewController.swift
+++ Quiz 2020/View Controllers/WelcomViewController.swift
@@ -13,6 +13,10 @@ class WelcomViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        print(#line, #function)
+    }
 
 
 }
